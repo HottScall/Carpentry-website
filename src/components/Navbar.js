f795632372f4a88365fb4777f7578b3f394bd044
Scott <Scotthall1983@hotmail.com>
2020-01-14 01:29:05 +0000

    Added photos to overall file. Completed basic navigation bar with links to each section. Added a header and the company logo.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,32 @@
 import React from "react";
+import "./Navbar.css";
+import Logo from "../images/logo.jpg";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div>
-        <header role="banner">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="index.html">
-                L.A Sadler Carpentry
-              </a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarsExample05"
-                aria-controls="navbarsExample05"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </div>
-          </nav>
-        </header>
+      <div className="navbar">
+        <ul id="nav">
+          <img id="logo" src={Logo} alt="logo" />
+          <li>
+            <h1>L.A Sadler Carpentry</h1>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Testimonials</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
       </div>
     );
   }
